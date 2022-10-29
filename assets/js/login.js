d7c5dbebfec08097aab2cb009d00145c37c17585
ibrahemshabanah2022 <113831449+ibrahemshabanah2022@users.noreply.github.com>
2022-10-29 03:26:21 +0200

    forms update

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,10 +2,10 @@ const elementRegex = {
   userName: /^[a-zA-Z]{3,}$/,
   password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
   phone: /01[0125][0-9]{8}$/,
+  confirmpassword: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
   email: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/,
 };
-const confirmpassword_regex =
-  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
 window.onload = function () {
   const elements = document.querySelectorAll("input");
 
@@ -18,17 +18,5 @@ window.onload = function () {
         element.className = "form-control is-invalid";
       }
     };
-  });
-
-  confirmpassword.addEventListener("input", (e) => {
-    if (
-      confirmpassword.value == password.value &&
-      confirmpassword.value.match(confirmpassword_regex)
-    ) {
-      console.log(1);
-      confirmpassword.className = "form-control is-valid";
-    } else {
-      confirmpassword.className = "form-control is-invalid";
-    }
   });
 };
