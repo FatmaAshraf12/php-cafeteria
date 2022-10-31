61985b86e0541c6c7d532ced0f09effa986b955f
AbdelrahmanRamadan92 <abdelrahmanfaisal92@gmail.com>
2022-10-31 18:45:58 +0200

    delete product wihout refresh

--- admin/products/delete.js
+++ admin/products/delete.js
@@ -1,11 +1,11 @@
 let deletbtns = document.querySelectorAll('.delete');
-console.log(deletbtns);
+// console.log(deletbtns);
 deletbtns.forEach((btn) => btn.addEventListener('click', delete_product));
 
 
 function delete_product() {
   const id = this.dataset.id;
-  console.log(id);
+      console.log(id)
   fetch(`http://localhost/last-cafe/admin/products/delete.php?id=${id}`)
     .then((response) => response.json())
     .then((data) => {
